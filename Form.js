7ec48c13e2dc5c190a98fa40c223ff72a827343b
S.Aravind <63240570+ARAVIND281@users.noreply.github.com>
2020-05-29 16:56:53 +0530

    Add files via upload

--- Form.js
+++ Form.js
@@ -2,25 +2,96 @@ class Form {
     constructor() {
         this.title = createElement('h1');
         this.title.html("SAVE THE MONKEY");
-        this.greeting = createElement('h2');
         this.userName = createInput("NAME");
-        this.playButton = createButton('watch video');
+        this.playButton = createButton('watch STORY');
+        this.aboutGame = createElement('h3');
+        this.aboutGame.html("WATCH STORY TO KNOW ABOUT THE GAME");
+        this.next1 = createButton('NEXT');
+        this.next2 = createButton('NEXT');
+        this.next3 = createButton('NEXT');
+        this.next4 = createButton('NEXT');
+        this.next1.hide();
+        this.next2.hide();
+        this.next3.hide();
+        this.next4.hide();
+        this.levels = createElement('h1')
+        this.level1 = createButton('1')
+        this.level1.hide();
+        this.playb = createButton('PLAY');
+        this.playb.hide();
+        this.s1 = loadImage("image/S1.png");
+        this.s2 = loadImage("image/S2.png");
+        this.s3 = loadImage("image/S3.png");
+        this.s4 = loadImage("image/S4.png");
+        this.s5 = loadImage("image/S5.png");
     }
 
     display() {
-        this.title.position(displayWidth / 2 - 60, 0);
-        this.userName.position(displayWidth / 2 - 60, displayHeight / 2 - 80);
-        this.playButton.position(displayWidth / 2 - 50, displayHeight / 2);
-        this.playButton.mousePressed(()=>{
+        this.title.position(displayWidth / 2 - 110, 0);
+        this.aboutGame.position(displayWidth / 2 - 150, displayHeight / 2 - 150);
+        this.userName.position(displayWidth / 2 - 55, displayHeight / 2 - 200);
+        this.playButton.position(displayWidth / 2, displayHeight / 2);
+        this.playButton.mousePressed(() => {
             this.title.hide();
             this.userName.hide();
             this.playButton.hide();
+            this.aboutGame.hide();
             player.name = this.userName.value();
-            this.greeting.html("hello " + player.name);
-            this.greeting.position(displayWidth / 2 - 50, displayHeight / 2);
-            
-        }); 
+            image(this.s1, 0, 0, displayWidth, displayHeight);
+            this.next1.show();
+            this.next1.position(displayWidth - 100, displayHeight - 50);
+            //intoSound.play();
+            this.next1.mousePressed(() => {
+                image(this.s2, 0, 0, displayWidth, displayHeight);
+                this.next1.hide();
+                this.next2.show();
+                this.next2.position(displayWidth - 100, displayHeight - 50);
+                this.next2.mousePressed(() => {
+                    image(this.s3, 0, 0, displayWidth, displayHeight);
+                    this.next2.hide();
+                    this.next3.show();
+                    this.next3.position(displayWidth - 100, displayHeight - 50);
+                    this.next3.mousePressed(() => {
+                        image(this.s4, 0, 0, displayWidth, displayHeight);
+                        this.next3.hide();
+                        this.next4.show();
+                        this.next4.position(displayWidth - 100, displayHeight - 50);
+                        this.next4.mousePressed(() => {
+                            image(this.s5, 0, 0, displayWidth, displayHeight);
+                            this.next4.hide();
+                            this.playb.show();
+                            this.playb.position(displayWidth / 2, displayHeight - 150);
+                            this.playb.mousePressed(()=>{
+                                this.playb.hide();
+                                clear();
+                                this.levels.html("LEVELS");
+                                this.levels.position(displayWidth / 2 - 50, 0);
+                                this.level1.show();
+                                this.level1.size(75,75);
+                                this.level1.position(10,100);
+                                this.level1.mousePressed(()=>{
+                                    this.level1.hide();
+                                    this.levels.hide();
+                                    lev = 1;
+                                })
+
+                            });
+
+                        });
+
+                    });
+                });
+
+
+            });
+
+
+
+        });
 
     }
 
+    
+
 }
+
--- sketch.js
+++ sketch.js
@@ -1,14 +1,43 @@
 var canvas;
-var form,player;
+var form,player1;
+var gameState = 0;
+var player,player_img;
+var track;
+var carImg1,carImg2,carimg3,carImg4,carImg45;
+var car,rand;
+var carsGroup;
+var Iwall1,Iwall2;
+var score = 0;
+var posY = -5000,posX = -500;
+var lev = 0;
 
 function preload() {
+    intoSound = loadSound('audio/intoaudio.mp3');
+    player_img = loadImage("images/player.png");
+  carImg1 = loadImage("images/car1.png");
+  carImg2 = loadImage("images/car2.jpg");
+  carImg3 = loadImage("images/car3.jpg");
+  carImg4 = loadImage("images/car4.jpg");
+  carImg5 = loadImage("images/car5.jpg");
+  track = loadImage("images/track.png");
 
 }
 
 function setup() {
     canvas = createCanvas(displayWidth, displayHeight);
     form = new Form();
-    player = new Player();
+    player1 = new Player();
+    player = createSprite(displayWidth/2,2300,40,40);
+  player.addImage(player_img);
+  player.scale = 2;
+
+  Iwall1 = createSprite(198,displayHeight/2,40,displayHeight*8);
+  Iwall2 = createSprite(1260,displayHeight/2,40,displayHeight*8);
+
+  carsGroup = new Group();
+  if(frameCount%40===0){
+    car = createSprite(posX,posY,30,30);
+  }
 
 
 }
@@ -17,7 +46,93 @@ function draw() {
 
     form.display();
 
+    if(lev === 1){
+        level1();
+
+    }
+
     drawSprites();
 
 
 } 
+
+function level1(){
+    background("#D8C7A9");
+  image(track, 0,-displayHeight*6,displayWidth+100, displayHeight*9);
+  camera.position.x = displayWidth/2;
+  camera.position.y = player.y-100; 
+
+  if(gameState===1){
+    fill("brown");
+    noStroke();
+    textSize(40);
+    text("You Lose!!!",displayWidth/4,player.y-300);
+  }
+
+  fill("brown");
+  noStroke();
+  textSize(20);
+  text("Your Score: "+ score,50,player.y-200);
+
+  Iwall1.visible = false;
+  Iwall2.visible = false;
+  player.collide(Iwall1);
+  player.collide(Iwall2);
+  player.velocityY = -10;
+
+  if(keyDown(LEFT_ARROW)){
+    player.x -= 10;
+  }
+  if(keyDown(RIGHT_ARROW)){
+    player.x += 10;
+  }
+  if(player.y === -4470){
+    gameState = 2;
+    player.velocityY = 0;
+    carsGroup.setVelocityXEach(0);
+    fill("brown");
+    noStroke();
+    textSize(40);
+    text("You Win!!!",displayWidth/4,player.y-300);
+
+  }
+  if(frameCount % 50 === 0 && gameState === 0){
+    score += 5;
+  }
+
+  if(frameCount % 50 === 0 && gameState === 0){
+    rand = random(220,1255);
+    car.y = player.y-800;
+    car.x = rand;
+    car.depth = player.depth;
+    car.collide(Iwall1);
+    car.collide(Iwall2);
+    num = Math.round(random(1,1));
+
+    switch(num) {
+      case 1: car.addImage(carImg1);
+              break;
+      case 2: car.addImage(carImg2);
+              break;
+      case 3: car.addImage(carImg3);
+              break;
+      case 4: car.addImage(carImg4);
+              break;
+      case 5: car.addImage(carImg5);
+              break;
+      default: break;
+    }
+
+    car.scale = 2;
+    car.velocityY = 10;
+    carsGroup.add(car);
+    car.lifetime = -100;
+  }
+  if(player.isTouching(car)){
+    gameState = 1;
+    player.destroy();
+    carsGroup.destroyEach();
+    score -= 5;
+  }
+
+}
